6595c040f149ba3f3628cfa8a4bf40ce4a3bcefa
Dimash <dinmukhamed.amirov@gmail.com>
2025-11-24 23:46:05 +0800

    fix: users drop db

--- src/routes/drop/index.ts
+++ src/routes/drop/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 
 import { BlogModel } from "../blogs/model";
 import { PostModel } from "../posts/model";
+import { UserModel } from "../users/model";
 
 import { HttpResponses } from "../../const";
 
@@ -11,6 +12,7 @@ dropRouter.delete("/", async (req: Request, res: Response) => {
   try {
     await BlogModel.deleteMany({});
     await PostModel.deleteMany({});
+    await UserModel.deleteMany({});
 
     return res.sendStatus(HttpResponses.NO_CONTENT);
   } catch (error) {
