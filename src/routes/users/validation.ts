5f1124c13601d358d178014463fa6a2d0a0ee98a
Dimash <dinmukhamed.amirov@gmail.com>
2025-11-27 13:19:47 +0800

    fix: validation

--- src/routes/users/validation.ts
+++ src/routes/users/validation.ts
@@ -6,14 +6,24 @@ export const userValidation = [
     .isString()
     .withMessage("login must be a string")
     .notEmpty()
-    .withMessage("login is required"),
+    .withMessage("login is required")
+    .isLength({ min: 3 })
+    .withMessage("login is too long")
+    .isLength({ max: 10 })
+    .withMessage("login is too long")
+    .matches(/^[a-zA-Z0-9_-]*$/)
+    .withMessage("login must contain only letters, numbers, _ or -"),
 
   body("password")
     .trim()
     .isString()
     .withMessage("password must be a string")
     .notEmpty()
-    .withMessage("password is required"),
+    .withMessage("password is required")
+    .isLength({ min: 6 })
+    .withMessage("password is too long")
+    .isLength({ max: 20 })
+    .withMessage("login is too long"),
 
   body("email")
     .trim()
